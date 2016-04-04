949b8db46eca3bd3ca17f20908cb5c93bb18a0e5
Kristjan <kristjan.mark@khk.ee>
2016-04-04 14:32:48 +0300

    Lecture 25, Lecture 26: Snake Game Scripting - Part A and B [Deliver #116708523]

--- snake_game/script.js
+++ snake_game/script.js
@@ -1,3 +1,45 @@
-/**
- * Created by Senka on 17.11.2015.
- */
+$(document).ready(function(){
+    //define vars
+    var canvas = $('#canvas')[0];
+    var ctx = canvas.getContext("2d");
+    var w = canvas.width();
+    var h = canvas.height();
+    var cw = 15;
+    var d = "right";
+    var food;
+    var score;
+    var speed = 100;
+
+
+    //Snake Array
+    var snake_array;
+
+    //initializer
+    function init() {
+        create_snake();
+        create_food();
+        score = 0;
+
+        if(typeof game_loop != "undefined") clearInterval (game_loop);
+        game_loop = setInterval(paint, speed);
+    }
+    init();
+
+    //Create Snake
+    function create_snake() {
+        var length = 5;
+        snake_array = [];
+        for(var i = length- 1; i>= 0; i--) {
+            snake_array.push({x: i, y:0});
+
+        }
+    }
+
+    //Create Food
+    function create_food() {
+        food = {
+            x:Math.round(Math.random()*(w-cw)/cw),
+            y:Math.round(Math.random()*(h-cw)/cw);
+    };
+}
+});
