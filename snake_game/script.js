42cb0af71515b0f8dc341460d170bd93dc7243c0
Kristjan <kristjan.mark@khk.ee>
2016-04-04 14:34:21 +0300

    Lecture 25, Lecture 26: Snake Game Scripting - Part A and B [Deliver #116708523]

--- snake_game/script.js
+++ snake_game/script.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
     //define vars
     var canvas = $('#canvas')[0];
     var ctx = canvas.getContext("2d");
-    var w = canvas.width();
-    var h = canvas.height();
+    var w = $("canvas").width();
+    var h = $("canvas").height();
     var cw = 15;
     var d = "right";
+    var color = "green";
     var food;
     var score;
     var speed = 100;
@@ -39,7 +40,67 @@ $(document).ready(function(){
     function create_food() {
         food = {
             x:Math.round(Math.random()*(w-cw)/cw),
-            y:Math.round(Math.random()*(h-cw)/cw);
-    };
-}
+            y:Math.round(Math.random()*(h-cw)/cw)
+        };
+    }
+
+    //Paint
+    function paint() {
+        //Paint the canvas
+        ctx.fillStyle = "black";
+        ctx.fillRect(0,0,w,h);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(0,0,w,h);
+
+        var nx = snake_array[0].x;
+        var ny = snake_array[0].y;
+
+        if(d == 'right') nx++;
+        else if(d == 'left') nx--;
+        else if(d == 'up') ny--;
+        else if(d == 'down') ny++;
+
+        //Collide script
+        if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array)) {
+            init();
+
+            return;
+        }
+
+        if(nx == food.x && ny == food.y) {
+            var tail = {x: nx, y: ny};
+            score++;
+            create_food();
+        } else {
+            var tail = snake_array.pop();
+            tail.x = nx; tail.y = ny;
+        }
+
+        snake_array.unshift(tail);
+
+        for(var i = 0; i < snake_array.length;i++) {
+            var c = snake_array[i];
+            paint_cell(c.x, c.y);
+        }
+
+        paint_cell(food.x, food.y);
+
+        //Check Score
+        checkscore(score);
+    }
+
+    function paint_cell(x,y) {
+        ctx.fillStyle = color;
+        ctx.fillRect(x*cw, y*cw, cw, cw);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(x*cw, y*cw, cw, cw);
+    }
+
+    function check_collision(x, y, array) {
+        for(var i = 0;i < array.length; i++) {
+            if(array[i.x == x && array[i].y == y])
+                return true;
+        }
+        return false;
+    }
 });
