67cdbcda446bb5206f6c37f8ec08983a8eb1c65c
Kristjan <kristjan.mark@khk.ee>
2016-04-04 14:49:24 +0300

    Lecture 38: Image Uploader UI - CSS [Deliver #116868189]

--- dd_uploader/js/main.js
+++ dd_uploader/js/main.js
@@ -1,3 +1,42 @@
-/**
- * Created by Senka on 22.11.2015.
- */
+$(function () {
+    var picbox = $('#picbox'),
+        back = $('.back', picbox);
+
+    picbox.filedrop({
+        paramname: 'pic',
+        maxfilesize: 2,
+        maxfiles: 6,
+        url: 'upload.php',
+
+        uploadFinished: function (file, response) {
+            $.data(file).addClass('done');
+        },
+
+        error: function (err, file) {
+            switch (err) {
+                case 'BroswerNotSuported':
+                    showMessage('Browser not supporting html5 file uploading');
+                    break;
+                case 'TooManyFiles':
+                    alert('Max number of files');
+                    break;
+                case 'FileTooLarge':
+                    alert(file.name + ' file size more than max');
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        beforeEach: function(file) {
+            if(!file.type.match(^/image\//)){
+                alert('File is not image');
+                return false;
+            }
+        },
+
+        uploadStart: function(i, file, len){
+
+        }
+    });
+});
