1e62303b722d98cf2b7924eb7ab9733151edcb21
Kristjan <kristjan.mark@khk.ee>
2016-04-04 15:12:57 +0300

    Lecture 55: Volume and Duration [Deliver #116868321]

--- audio_player/js/main.js
+++ audio_player/js/main.js
@@ -1,3 +1,47 @@
-/**
- * Created by Kristjan on 26.11.2015.
- */
+var audio = new Audio('media/Linkin Park - By Myself.mp3');
+
+//Hide Button
+$('#pause').hide();
+
+//Player
+$('#play').click(function(){
+    audio.play();
+    $('#pause').show();
+    $('#play').hide();
+    showDuration();
+});
+$('#pause').click(function(){
+    audio.pause();
+    $('#play').show();
+    $('#pause').hide();
+});
+$('#stop').click(function() {
+    audio.pause();
+    $('#pause').hide();
+    $('#play').show();
+    audio.currentTime = 0;
+});
+
+
+//Volume
+$('#volume').change(function(){
+    audio.volume = parseFloat(this.value / 10);
+});
+
+
+//Audio Player Time Duration
+function showDuration(){
+    $(audio).bind('timeupdate', function(){
+        var s  = parseInt(audio.currentTime % 60);
+        var min = parseInt(audio.currentTime / 60) % 60;
+        if(s < 10) {
+            s = '0' + s;
+        }
+        $('#duration').html(min + ':' + s);
+        var value = 0;
+        if(audio.currentTime > 0){
+            value = Math.floor((100 / audio.duration) * audio.currentTime);
+        }
+        $('#progress').css('width', value+'%');
+    });
+}
