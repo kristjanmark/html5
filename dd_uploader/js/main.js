92aafaa99e01cb162d157b726d3f0424beb65c4e
Kristjan <kristjan.mark@khk.ee>
2016-04-04 14:52:55 +0300

    Lecture 42: PHP upload file Part - B [Deliver #116868227]

--- dd_uploader/js/main.js
+++ dd_uploader/js/main.js
@@ -1,68 +1,68 @@
-$(function () {
+$(function(){
     var picbox = $('#picbox'),
         back = $('.back', picbox);
 
     picbox.filedrop({
-        paramname: 'pic',
+        paramname:'pic',
         maxfilesize: 2,
         maxfiles: 6,
         url: 'upload.php',
 
-        uploadFinished: function (file, response) {
+        uploadFinished: function(i,file,response){
             $.data(file).addClass('done');
+            $('.uploaded').show();
         },
-
-        error: function (err, file) {
-            switch (err) {
-                case 'BroswerNotSuported':
-                    showMessage('Browser not supporting html5 file uploading');
+        error: function(err, file){
+            switch(err){
+                case 'BrowserNotSupported':
+                    showMessage('Your browser does not support HTML5 file uploads');
                     break;
                 case 'TooManyFiles':
-                    alert('Max number of files');
+                    alert('You went over the max number of files');
                     break;
                 case 'FileTooLarge':
-                    alert(file.name + ' file size more than max');
+                    alert(file.name+' is too big, please upload a smaller image');
                     break;
                 default:
                     break;
             }
         },
 
-        beforeEach: function (file) {
-            if (!file.type.match(/image\//))
-            {
-                alert('File is not image');
+        beforeEach: function(file){
+            if(!file.type.match(/^image\//)){
+                alert('Your file is not an image');
                 return false;
             }
         },
-
-        uploadStart: function (i, file, len) {
+        uploadStarted: function(i, file,len){
             createImage(file);
         },
-
-        progressUpdated: function (i, file, progress) {
+        progressUpdated: function(i, file, progress){
             $.data(file).find('.progress').width(progress);
         }
     });
 
-    var template = '<div class="preview">' +
-        '<span class="imageHolder">' +
-        '<img />' +
-        '</span>' +
-        '<div class="progressHolder">' +
-        '<div class="progress"></div>' +
-        '</div>' +
+    var template = '<div class="preview">'+
+        '<span class="imageHolder">'+
+        '<span class="uploaded"></span>'+
+        '<img />'+
+        '</span>'+
+        '<div class="progressHolder">'+
+        '<div class="progress"></div>'+
+        '</div>'+
         '</div>';
-    function createImage(file) {
+
+    function createImage(file){
         var preview = $(template),
-            image = $('img', preview);
+            image = $('img',preview);
+
         var reader = new FileReader();
 
         image.width = 100;
         image.height = 100;
 
-        reader.onload = function(e) {
-            image.attr('src', e.target.result);
+        reader.onload = function(e){
+            image.attr('src',e.target.result);
         };
 
         reader.readAsDataURL(file);
@@ -70,10 +70,10 @@ $(function () {
         back.hide();
         preview.appendTo(picbox);
 
-        $.data(file, preview);
+        $.data(file,preview);
     }
 
-    function showMessage(msg) {
+    function showMessage(msg){
         back.html(msg);
     }
 });
