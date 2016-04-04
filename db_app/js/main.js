9256a89bb2f94d1d6bf96ad134c1497e828e5995
Kristjan <kristjan.mark@khk.ee>
2016-04-04 15:30:28 +0300

    Lecture 68: Database [Deliver #116868423]

--- db_app/js/main.js
+++ db_app/js/main.js
@@ -1,3 +1,27 @@
-/**
- * Created by Senka on 01.12.2015.
- */
+$(document).ready(function () {
+//Open Database
+    var request = indexedDB.open('customermanager', 1);
+
+    //Success
+    request.onsuccess = function (e) {
+        console.log('Success: Opened Database');
+        db = e.target.result;
+        //Show Customers
+        showCustomers();
+    }
+    //Error
+    request.onerror = function (e) {
+        console.log('Error');
+
+    }
+
+    request.onupgradeneeded = function() {
+        var db = e.target.result();
+
+        if(!db.objectStoreName.contains('customers')) {
+            var os = db.createObjectStore('customers', {keyPath: "id", autoIncrement:true});
+        }
+    }
+
+
+});
