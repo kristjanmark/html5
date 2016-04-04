15122f4aea30d4f64d745f753ac441502f7d18e6
Kristjan <kristjan.mark@khk.ee>
2016-04-04 15:15:16 +0300

    Lecture 57: Playlist Functionality Part - B [Deliver #116868343]

--- audio_player/js/main.js
+++ audio_player/js/main.js
@@ -2,7 +2,7 @@ var audio;
 //Hide Button
 $('#pause').hide();
 
-initAudio('#playist li:first-child');
+initAudio($('#playlist li:first-child'));
 
 function initAudio(element) {
     var song = element.attr('song');
@@ -11,58 +11,89 @@ function initAudio(element) {
     var artist = element.attr('artist');
 
     //Create Audio
-    audio = new Audio('media/'+ song);
+    audio = new Audio('media/' + song);
 
     //Audio info append to div
     $('.artist').text(artist);
     $('.title').text(title);
 
     //Song cover
-    $('img.cover').attr('src','images/covers/'+cover);
+    $('img.cover').attr('src', 'img/covers/' + cover);
 
     $('#playlist li').removeClass('active');
     element.addClass('active');
 }
 
 //Player
-$('#play').click(function(){
+$('#play').click(function () {
     audio.play();
     $('#pause').show();
     $('#play').hide();
     showDuration();
 });
-$('#pause').click(function(){
+$('#pause').click(function () {
     audio.pause();
     $('#play').show();
     $('#pause').hide();
 });
-$('#stop').click(function() {
+$('#stop').click(function () {
     audio.pause();
-    $('#pause').hide();
-    $('#play').show();
     audio.currentTime = 0;
 });
 
+//Next button
+$('#next').click(function () {
+    audio.pause();
+    var next = $('#playlist li.active').next();
+    if (next.length == 0) {
+        next = $('#playlist li:first-child');
+    }
+    initAudio(next);
+    audio.play();
+    showDuration();
+});
+
+//Prev button
+$('#prev').click(function () {
+    audio.pause();
+    var prev = $('#playlist li.active').prev();
+    if (prev.length == 0) {
+        prev = $('#playlist li:last-child');
+    }
+    initAudio(prev);
+    audio.play();
+    showDuration();
+});
+
+//Playlist song click
+$('#playlist li').click(function(){
+    audio.pause();
+    initAudio($(this));
+    $('#play').hide();
+    $('#pause').show();
+    audio.play();
+    showDuration();
+});
 
 //Volume
-$('#volume').change(function(){
+$('#volume').change(function () {
     audio.volume = parseFloat(this.value / 10);
 });
 
 
 //Audio Player Time Duration
-function showDuration(){
-    $(audio).bind('timeupdate', function(){
-        var s  = parseInt(audio.currentTime % 60);
+function showDuration() {
+    $(audio).bind('timeupdate', function () {
+        var s = parseInt(audio.currentTime % 60);
         var min = parseInt(audio.currentTime / 60) % 60;
-        if(s < 10) {
+        if (s < 10) {
             s = '0' + s;
         }
         $('#duration').html(min + ':' + s);
         var value = 0;
-        if(audio.currentTime > 0){
+        if (audio.currentTime > 0) {
             value = Math.floor((100 / audio.duration) * audio.currentTime);
         }
-        $('#progress').css('width', value+'%');
+        $('#progress').css('width', value + '%');
     });
 }
